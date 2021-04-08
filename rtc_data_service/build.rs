812356e25ee4ea78477a8ceeb3d278450b9f39d4
Herman <herman@registree.io>
2021-04-08 11:59:14 +0200

    chore(build): cleanup build and test build

--- rtc_data_service/build.rs
+++ rtc_data_service/build.rs
@@ -15,9 +15,8 @@ fn main() {
             "cargo:rustc-link-search=native={}/build/data_system/lib",
             cur_dir.parent().unwrap().to_str().unwrap()
         );
-        println!("cargo:rustc-link-lib=static=Enclave_u");
 
-        println!("cargo:rustc-link-lib=dylib=sgx_dcap_ql");
+        println!("cargo:rustc-link-lib=static=Enclave_u");
 
         println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
         println!("cargo:rustc-link-lib=static=sgx_uprotected_fs");
