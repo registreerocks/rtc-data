d5adc8989a44d70d8a42ec338568e244ee081841
Pi Delport <pi@registree.io>
2021-05-06 20:02:33 +0200

    refactor(rtc_data_service) drop unused mut

--- rtc_data_service/http_server/src/tls.rs
+++ rtc_data_service/http_server/src/tls.rs
@@ -13,7 +13,7 @@ use std::{fs, io};
 pub fn get_tls_server_config(config: TlsConfig) -> Result<TlsServerConfig, TlsConfigError> {
     let client_auth = match config.client_cert_path {
         Some(path) => {
-            let mut roots = load_certs(&path)?;
+            let roots = load_certs(&path)?;
             let mut client_auth_roots = RootCertStore::empty();
             for root in roots {
                 client_auth_roots.add(&root)?;
