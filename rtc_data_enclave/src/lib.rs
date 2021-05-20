e11cc2b09e329de55bb644e745ebb5f6104e1935
Pi Delport <pi@registree.io>
2021-05-20 20:17:12 +0200

    check(rtc_data_enclave): unused feature(const_generics) and feature(const_evaluatable_checked)
    These usages were moved to rtc_tenclave.

--- rtc_data_enclave/src/lib.rs
+++ rtc_data_enclave/src/lib.rs
@@ -3,8 +3,6 @@
 #![crate_type = "staticlib"]
 #![cfg_attr(not(target_env = "sgx"), no_std)]
 #![cfg_attr(target_env = "sgx", feature(rustc_private))]
-#![feature(const_generics)]
-#![feature(const_evaluatable_checked)]
 #![deny(clippy::mem_forget)]
 // TODO: Clean up existing cases causing a flood of warnings for this check, and re-enable
 // #![warn(missing_docs)]
