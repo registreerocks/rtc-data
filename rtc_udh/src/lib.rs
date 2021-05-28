6e0f8be7592bd0dfca0e38570be47bc14fcd2c84
Pi Delport <pi@registree.io>
2021-05-28 15:13:04 +0200

    refactor(rtc_udh): get_responder: take enclave id by value, not reference
    This is more consistent with the surrounding code.

--- rtc_udh/src/lib.rs
+++ rtc_udh/src/lib.rs
@@ -39,11 +39,11 @@ pub fn set_responder(
     }
 }
 
-fn get_responder(id: &u64) -> Result<SyncSendResponder, sgx_status_t> {
+fn get_responder(id: u64) -> Result<SyncSendResponder, sgx_status_t> {
     dh_responders()
         .read()
         .or(Err(sgx_status_t::SGX_ERROR_UNEXPECTED))?
-        .get(id)
+        .get(&id)
         .ok_or(sgx_status_t::SGX_ERROR_INVALID_ENCLAVE_ID)
         .map(Clone::clone)
 }
@@ -55,7 +55,7 @@ pub extern "C" fn rtc_session_request_u(
 ) -> SessionRequestResult {
     // TODO: Refactor our duplicated code here, this is tricky because of variable
     // scoping when working with locks
-    match get_responder(&dest_enclave_id) {
+    match get_responder(dest_enclave_id) {
         Ok(res) => res
             .lock()
             .or(Err(sgx_status_t::SGX_ERROR_UNEXPECTED))
@@ -71,7 +71,7 @@ extern "C" fn rtc_exchange_report_u(
     dest_enclave_id: sgx_enclave_id_t,
     dh_msg2: *const sgx_dh_msg2_t,
 ) -> ExchangeReportResult {
-    match get_responder(&dest_enclave_id) {
+    match get_responder(dest_enclave_id) {
         Ok(res) => res
             .lock()
             .or(Err(sgx_status_t::SGX_ERROR_UNEXPECTED))
@@ -86,7 +86,7 @@ extern "C" fn rtc_end_session_u(
     src_enclave_id: sgx_enclave_id_t,
     dest_enclave_id: sgx_enclave_id_t,
 ) -> sgx_status_t {
-    match get_responder(&dest_enclave_id) {
+    match get_responder(dest_enclave_id) {
         Ok(res) => res
             .lock()
             .map_or(sgx_status_t::SGX_ERROR_UNEXPECTED, |resp| {
