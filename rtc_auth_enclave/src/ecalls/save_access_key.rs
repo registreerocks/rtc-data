f26b1b68b6944bcdb9a4f5bd2882f0897ecef0ed
Pi Delport <pi@registree.io>
2021-06-03 19:55:59 +0200

    WIP: flesh out save_access_key

--- rtc_auth_enclave/src/ecalls/save_access_key.rs
+++ rtc_auth_enclave/src/ecalls/save_access_key.rs
@@ -1,6 +1,8 @@
 //! ECALL definition: [`save_access_key`]
 
-use std::dbg;
+use std::{dbg, format};
+
+use sgx_types::sgx_enclave_id_t;
 
 use rtc_types::enclave_messages::ng_set_access_key;
 use rtc_types::enclave_messages::set_access_key;
@@ -22,12 +24,30 @@ pub extern "C" fn save_access_key(
 fn save_access_key_impl(
     encrypted_request: set_access_key::EncryptedRequest,
 ) -> set_access_key::EncryptedResponse {
-    dbg!(
-        encrypted_request.tag,
-        encrypted_request.ciphertext,
-        encrypted_request.aad,
-        encrypted_request.nonce,
-    );
+    // FIXME: Get sending enclave ID via AAD?
+    let dummy_enclave_id = sgx_enclave_id_t::default();
+
+    let other_enclave_id = dummy_enclave_id;
+
+    let sessions: &crate::DhSessions<_, _> = crate::dh_sessions();
+    let channel_mutex = sessions.get_active(other_enclave_id).expect(&format!(
+        "save_access_key_impl: no active DH session for sending enclave {:?}",
+        other_enclave_id,
+    ));
+    let channel = channel_mutex
+        .lock()
+        .expect("save_access_key_impl: protected channel mutex poisoned");
+
+    let request_bytes = channel
+        .decrypt_message(encrypted_request)
+        .expect("TODO: return Result<>");
+    dbg!(request_bytes);
+    // TODO: deserialize,
+
+    let response = set_access_key::Response { success: false };
+    dbg!(response);
+    // TODO: serialize
+
     set_access_key::EncryptedResponse {
         tag: Default::default(),
         ciphertext: Default::default(),
