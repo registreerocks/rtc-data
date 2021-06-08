8d22a9a301d15c6ecf7093510d833be485eac6ab
Pi Delport <pi@registree.io>
2021-06-08 12:30:29 +0200

    style(rtc_data_service,exec): move ResponseBody up (to match other service modules)

--- rtc_data_service/src/exec/service.rs
+++ rtc_data_service/src/exec/service.rs
@@ -41,6 +41,15 @@ pub mod models {
         pub nonce: Vec<u8>,
     }
 
+    #[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+    pub struct ResponseBody {}
+
+    impl From<RequestExecutionResponse> for ResponseBody {
+        fn from(_resp: RequestExecutionResponse) -> Self {
+            todo!()
+        }
+    }
+
     #[derive(Serialize, Deserialize, Debug)]
     pub struct RequestBody {
         pub metadata: Metadata,
@@ -52,15 +61,6 @@ pub mod models {
         type Error = ValidationError;
 
         fn try_from(_value: RequestBody) -> Result<Self, Self::Error> {
-            todo!()
-        }
-    }
-
-    #[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
-    pub struct ResponseBody {}
-
-    impl From<RequestExecutionResponse> for ResponseBody {
-        fn from(_resp: RequestExecutionResponse) -> Self {
             todo!()
         }
     }
