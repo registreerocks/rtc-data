38659590d8de10b7633b9fd96a4e1805a32de088
Herman <herman@registree.io>
2021-06-17 16:14:51 +0200

    feat(uenclave): add token issuance functions for auth enclave

--- rtc_uenclave/src/enclaves/rtc_auth.rs
+++ rtc_uenclave/src/enclaves/rtc_auth.rs
@@ -2,6 +2,7 @@ use std::borrow::Borrow;
 
 use crate::{AttestationError, EnclaveConfig, EnclaveReportResult, RtcEnclave};
 use auth_sys::AuthSys;
+use rtc_types::{EcallError, EncryptedMessage, ExecReqMetadata, ExecTokenError};
 use sgx_types::*;
 
 /// Wraps all the functionality for interacting with the auth enclave
@@ -46,5 +47,50 @@ where
     /// Get the id of this enclave instance
     pub fn geteid(&self) -> sgx_enclave_id_t {
         self.0.geteid()
+    }
+
+    /// Issues an execution token using the provided payload
+    pub fn issue_execution_token(
+        &self,
+        payload: &[u8],
+        metadata: ExecReqMetadata,
+    ) -> Result<EncryptedMessage, EcallError<ExecTokenError>> {
+        ecalls::issue_execution_token(self.geteid(), payload, metadata)
+    }
+}
+
+pub mod ecalls {
+    use auth_sys::ffi;
+    use rtc_types::*;
+    use sgx_types::*;
+
+    pub fn issue_execution_token(
+        eid: sgx_enclave_id_t,
+        payload: &[u8],
+        metadata: ExecReqMetadata,
+    ) -> Result<EncryptedMessage, EcallError<ExecTokenError>> {
+        let mut out_token_buffer = vec![0u8; 416].into_boxed_slice();
+        let mut retval = IssueTokenResult::Ok([0u8; 24]);
+
+        // Safety: Since the payload, and out buffer is allocated and valid this will be
+        // correctly handled by the SGX edge code
+        let res = unsafe {
+            ffi::rtc_auth_issue_execution_token(
+                eid,
+                &mut retval,
+                payload.as_ptr(),
+                payload.len(),
+                &metadata,
+                out_token_buffer.as_mut_ptr(),
+                out_token_buffer.len(),
+            )
+        };
+
+        let x: Result<Nonce, EcallError<ExecTokenError>> = retval.to_ecall_err(res).into();
+
+        x.map(|r| EncryptedMessage {
+            ciphertext: out_token_buffer,
+            nonce: r,
+        })
     }
 }
