02aedb8cf9807b9fc585fad2e6a919551a25a2ac
Pi Delport <pi@registree.io>
2021-07-07 22:33:03 +0200

    chore(rtc_uenclave/data-ocalls): drop template tests

--- rtc_uenclave/data-ocalls/src/lib.rs
+++ rtc_uenclave/data-ocalls/src/lib.rs
@@ -55,11 +55,3 @@ fn save_sealed_blob_to_fs(uuid: Uuid, blob: &[u8]) -> Result<(), String> {
 
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
